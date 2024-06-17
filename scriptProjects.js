f7f3c9651a6f25c5bcddffb3f3f9ed3243c0723e
io <103241346+Moksja@users.noreply.github.com>
2024-06-17 12:17:09 +0200

    Update scriptProjects.js
    MAJ ordre projets

--- scriptProjects.js
+++ scriptProjects.js
@@ -516,10 +516,10 @@ class CodeProject {
     }
 }
 let codeProjectsList = [
-    new CodeProject("images/codes/chatons.png", "Chaton Totem", "https://moksja.github.io/catotem/index.html"),
-    new CodeProject("images/codes/animal/animal2.png", "Animal Totem", "https://moksja.github.io/animaltotem/"),
-    new CodeProject("images/codes/portfolio.png", "Portfolio",""),
     new CodeProject("images/codes/neko.png", "Neko Sagashi", "https://moksja.github.io/nekoSagashi/"),
+    new CodeProject("images/codes/portfolio.png", "Portfolio",""),
+    new CodeProject("images/codes/animal/animal2.png", "Animal Totem", "https://moksja.github.io/animaltotem/"),
+    new CodeProject("images/codes/chatons.png", "Chaton Totem", "https://moksja.github.io/catotem/index.html"),
     new CodeProject("images/games/avenir/avenir1.png", "À venir", "")
 ]
 function addCodeCard() {
@@ -566,9 +566,10 @@ function addCodeCard() {
 function addCodeDescription() {
     let container1 = document.getElementById("codeDescr0");
     let descriptions1 = [
-        "Quel chaton es-tu aujourd'hui ?",
-        "Programme d'entraînement : Randomization.", 
-        "C'est mon premier programme. À chaque itération, l'algorithme choisit un objet au hasard."
+        "Jeu de rapidité - En cours de programmation", 
+        "Petit jeu dont l'objectif est de retrouver les 3 chatons parmi tous ceux à l'écran. ",
+        "La grille de chat est randomisée à partir d'une liste plus grande. Quand on clique sur un chaton, s'il est parmi ceux à retrouver, il disparaît et on gagne des points, sinon, on en perd et il reste affiché. Il y a également un chronomètre.",
+        "À rajouter : Plusieurs niveaux de difficulté ; Scènes animées de début et fin de manche."
     ] 
     descriptions1.forEach(description => {
         let desc = document.createElement("div");
@@ -579,10 +580,7 @@ function addCodeDescription() {
 
     let container2 = document.querySelector("#codeDescr1");
     let descriptions2 = [
-        "Quel est ton animal totem ?", 
-        "Programme de reconnaissance d'images reposant sur l'utilisation de Teachable Machine. ",
-        "J'ai entraîné le programme avec 5 animaux, et via la caméra, l'algorithme choisit celui qui nous ressemble le plus, notre totem.",
-        "Le traitement de l'image prend un peu de temps, et la classe Axolot semble sortir plus souvent, c'est sans doute dû à l'apparence des animaux choisis, difficile à retrouver dans les visages humains."
+        "Site conçu en ~1semaine, avec pour objectif de montrer mon envie d'apprendre et ma capacité à être autonome."
     ] 
     descriptions2.forEach(description => {
         let desc = document.createElement("div");
@@ -593,7 +591,11 @@ function addCodeDescription() {
 
     let container3 = document.querySelector("#codeDescr2");
     let descriptions3 = [
-        "Site conçu en ~1semaine, avec pour objectif de montrer mon envie d'apprendre et ma capacité à être autonome."
+        "Quel est ton animal totem ?", 
+        "Programme de reconnaissance d'images reposant sur l'utilisation de Teachable Machine. ",
+        "J'ai entraîné le programme avec 5 animaux, et via la caméra, l'algorithme choisit celui qui nous ressemble le plus, notre totem.",
+        "Le traitement de l'image prend un peu de temps, et la classe Axolot semble sortir plus souvent, c'est sans doute dû à l'apparence des autres animaux choisis, plus difficiles à retrouver dans les visages humains."
+
     ] 
     descriptions3.forEach(description => {
         let desc = document.createElement("div");
@@ -618,7 +620,10 @@ function addCodeDescription() {
 
     let container5 = document.querySelector("#codeDescr4");
     let descriptions5 = [
-        "Un jeu d'ambiance en ligne, conçu avec un ami pour nos proches. Sous forme de visual novel à choix multiples et divers mini-jeux."
+       "Quel chaton es-tu aujourd'hui ?",
+        "Programme d'entraînement : Randomization.", 
+        "C'est mon premier programme avec JavaScript. À chaque itération, l'algorithme choisit un objet au hasard."
+
     ] 
     descriptions5.forEach(description => {
         let desc = document.createElement("div");
